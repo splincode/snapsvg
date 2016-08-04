5cb449e64f06f4c93becaeb07862af1a815dd4d5
splincode <omaxphp@yandex.ru>
2016-08-04 21:16:46 +0300

    update

---
+++ demo/app.js
@@ -0,0 +1,11 @@
+var s = Snap("#draw");
+
+/*// Circle with 80px radius
+var circle = s.circle(90,120,80);
+// Square with 160px side width
+var square = s.rect(210,40,160,160);
+// Ellipse with 80px vertical radius and 50px horizontal radius
+var ellipse = s.ellipse(460,120,50,80);
+*/
+
+console.log(Snap.getRGB('#f5f5f5'))
